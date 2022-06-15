4214090ef1e50bc1eeb2f0b4e217101e71066f5f
kwelimarvo <marvin@kwelicapital.com>
2022-06-15 16:22:48 +0300

    Navbar Improved

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+
+// const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: ["./dist/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'pacifico': ["'Pacifico'", 'cursive'],
+        'montserrat': ["'Montserrat'", 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
